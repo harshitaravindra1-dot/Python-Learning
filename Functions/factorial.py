f6adbc6ce8940600cd92535068f625a642978822
Harshita R <harshitaravindra1@gmail.com>
2026-06-27 21:18:02 +0530

    Add factorial function

---
+++ Functions/factorial.py
@@ -0,0 +1,18 @@
+def factorial(n):
+    
+    if n == 0:
+        return 1
+    
+    if n < 0:
+        return 'Not Defined'
+
+    fact = 1
+    for i in range(1,n+1):
+        fact *= i
+
+    return fact
+
+num = int(input("Enter a number : "))
+result = factorial(num)
+
+print(f'Factorial of {num} = {result}')
